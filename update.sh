7836cc8ae4a0a214e184b40b60fcf0cc1dd57e66
Alex Ntavelos <a.ntavelos@gmail.com>
2022-05-06 15:21:06 +0200

    remove static volume upon update

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker-compose down && \
+docker volume rm predicted-runway-deploy_static && \
 docker-compose pull && \
 docker images | grep none | awk '{print $3}' | xargs -r docker rmi && \
 docker-compose up -d
