95bac73fed68417ee7e1089358bde7b58df27d13
Alex Ntavelos <a.ntavelos@gmail.com>
2022-06-22 12:07:36 +0200

    change predicted-runway-ui dist directory

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ docker-compose down && \
 docker-compose pull && \
 docker pull ghcr.io/eurocontrol-swim/predicted-runway-ui:main && \
 docker images | grep none | awk '{print $3}' | xargs -r docker rmi && \
-docker run -w /app --volume /data/pr-ui/html/:/app/dist ghcr.io/eurocontrol-swim/predicted-runway-ui:main npm run build && \
+docker run -w /app --volume "$(pwd)"/predicted_runway/pr-ui/html/:/app/dist ghcr.io/eurocontrol-swim/predicted-runway-ui:main npm run build && \
 docker-compose up -d
