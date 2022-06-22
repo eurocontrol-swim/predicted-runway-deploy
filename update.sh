ba4c212c46163409860e0b608bcf51b6120bf8b9
Alex Ntavelos <a.ntavelos@gmail.com>
2022-06-22 11:15:59 +0200

    remove dir creation from update script

--- update.sh
+++ update.sh
@@ -3,6 +3,5 @@
 docker-compose down && \
 docker-compose pull && \
 docker images | grep none | awk '{print $3}' | xargs -r docker rmi && \
-mkdir -p /data/pr-ui/html && \
 docker run -w /app --volume /data/pr-ui/html/:/app/dist ghcr.io/eurocontrol-swim/predicted-runway-ui:main npm run build && \
 docker-compose up -d
