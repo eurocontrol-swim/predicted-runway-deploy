cb163fb06d0dc4805c2e772ef0cab227372161c8
Alex Ntavelos <a.ntavelos@gmail.com>
2022-06-22 12:00:22 +0200

    include predicted-runway-ui image pull in update script

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 docker-compose down && \
 docker-compose pull && \
+docker pull ghcr.io/eurocontrol-swim/predicted-runway-ui:main && \
 docker images | grep none | awk '{print $3}' | xargs -r docker rmi && \
 docker run -w /app --volume /data/pr-ui/html/:/app/dist ghcr.io/eurocontrol-swim/predicted-runway-ui:main npm run build && \
 docker-compose up -d
